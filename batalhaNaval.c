abc50e30a8bb18ce09759034a4c1b26a1e84a1e8
GuilhermeBTO <guilherme190301@gmail.com>
2025-05-09 22:18:13 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,16 +4,16 @@ int main() {
     int tabuleiro[5][5] = {0};
 
     // Posiciona navio horizontalmente na linha 2, colunas 1-3
-    int linha_h = 1; // linha 2 (índice 1)
+    int linha_2 = 1; // linha 2 (índice 1)
     for (int j = 0; j < 3; j++) {
-        tabuleiro[linha_h][j] = 1;
+        tabuleiro[linha_2][j] = 1;
         printf("Navio horizontal: (%d, %d)\n", linha_h + 1, j + 1);
     }
 
     // Posiciona navio verticalmente na coluna 5, linhas 3-5
-    int coluna_v = 4; // coluna 5 (índice 4)
+    int coluna_e = 4; // coluna 5 (índice 4)
     for (int i = 2; i < 5; i++) {
-        tabuleiro[i][coluna_v] = 2;
+        tabuleiro[i][coluna_e] = 2;
         printf("Navio vertical: (%d, %d)\n", i + 1, coluna_v + 1);
     }
 
