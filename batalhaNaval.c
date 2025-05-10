36753abd1096a0a62fb3bbbc06064519f6f896b9
GuilhermeBTO <guilherme190301@gmail.com>
2025-05-10 12:18:50 -0300

    Aventureiro batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,21 +1,60 @@
 #include <stdio.h>
+#define LINHAS 10
+#define COLUNAS 10
 
 int main() {
-    int tabuleiro[5][5] = {0};
+    
+   // char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+   //printf("%c\t", letras[10]);
 
-    // Posiciona navio horizontalmente na linha 2, colunas 1-3
-    int linha_2 = 1; // linha 2 (índice 1)
-    for (int j = 0; j < 3; j++) {
-        tabuleiro[linha_2][j] = 1;
-        printf("Navio horizontal: (%d, %d)\n", linha_h + 1, j + 1);
+    int matriz[LINHAS][COLUNAS] = {0};
+    
+    //navio na horizontal
+    int linha_navio1 = 7;   // indice 7, 8 linha
+    int coluna_navio1 = 2;   // coluna C
+    int tamanho_navio1 = 3;
+ 
+    for (int i = linha_navio1; i < linha_navio1 + tamanho_navio1; i++) {
+        for (int j = coluna_navio1; j < coluna_navio1 + tamanho_navio1; j++) {
+            matriz[linha_navio1][j] = 3; // parte do navio.
+        }
     }
 
-    // Posiciona navio verticalmente na coluna 5, linhas 3-5
-    int coluna_e = 4; // coluna 5 (índice 4)
-    for (int i = 2; i < 5; i++) {
-        tabuleiro[i][coluna_e] = 2;
-        printf("Navio vertical: (%d, %d)\n", i + 1, coluna_v + 1);
+     //navio na vertical
+    int linha_navio2 = 0;   // indice 0, 1 linha
+    int coluna_navio2 = 9;   // coluna J
+    int tamanho_navio2 = 3;
+ 
+    for (int i = linha_navio2; i < linha_navio2 + tamanho_navio2; i++) {
+        for (int j = coluna_navio2; j < coluna_navio2 + tamanho_navio2; j++) {
+            matriz[i][coluna_navio2] = 3; // parte do navio.
+        }
     }
 
+    //navio na diagonal da esquerda para direita descendo
+    int linha_navio3 = 5;   // indice 5, 6 linha
+    int coluna_navio3 = 6;   // coluna F
+    int tamanho_navio3 = 3;
+ 
+    for (int i = 0; i < tamanho_navio3; i++) {
+        matriz[linha_navio3 + i][coluna_navio3 + i] = 3; // parte do navio.
+    } 
+    
+    //navio na diagonal da direita para esquerda descendo
+    int linha_navio4 = 1;   // indice 3, 4 linha
+    int coluna_navio4 = 3;   // coluna C
+    int tamanho_navio4 = 3;
+ 
+    for (int i = 0; i < tamanho_navio4; i++) {
+        matriz[linha_navio4 + i][coluna_navio4 - i] = 3; // parte do navio, atribuindo valor à matriz.
+    }   
+    
+    //exibição da matriz completa
+    for (int i = 0; i < LINHAS; i++ ) {
+        for (int j = 0; j < COLUNAS; j++) {
+        printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
